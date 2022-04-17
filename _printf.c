78f351162fc4f9fabae42e442ecadcfcaf726166
Kolejojoses <kolejojoses@gmail.com>
2022-04-17 12:22:09 -0700

    second commit

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf: coding printf function
+ * @format: format string containing the characters and the specifiers
+ * Return: formatted output string
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
