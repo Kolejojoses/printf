748f6f4a96b5b18f6076876d8076c58a84c605dc
Kolejojoses <kolejojoses@gmail.com>
2022-04-17 13:34:32 -0700

    1

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * _printf: coding printf function
@@ -9,5 +11,30 @@
 
 int _printf(const char *format, ...)
 {
+	const char *ptr;
+	char *str;
+	unsigned int i;
 
+	va_list arg;
+	va_start(arg, format);
+	for (ptr = format; *ptr != '\0'; ptr++)
+	{
+		while (*ptr != '%')
+		{
+			putchar(*ptr);
+			ptr++;
+		}
+		ptr++;
+		switch(*ptr)
+		{
+			case 'c': i = va_arg(arg, int);
+				  return(putchar(i));
+				  break;
+			case 's': str = va_arg(arg, char *);
+				  return(puts(str));
+				  break;
+		}
+	}
+	va_end(arg);
+	return (1);
 }
